2154d3c8286a149f159513f2cb8d8504ddeb86f0
WitriMaryani <151610233@smkassalaambandung.sch.id>
2017-08-07 17:05:52 +0700

    toko

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                <center>
+                    <h2>Selamat Datang di Tokoku</h2>
+                    <img src="qq.png">
+                    <h2>Bekerja keras untuk sukses dan pantang menyerah</h2>
+                    <h2>Tak berusaha tidak akan sukses</h2>  
+                </center>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
